85294ed27e4398cf5a7cd036a595fdacae83acf0
Hayeszy <1282429235@qq.com>
2022-06-27 22:24:09 +0800

    更新了加载less文件,加载器 - 处理图片

--- src/main.js
+++ src/main.js
@@ -11,5 +11,20 @@ import './tabs.js'
 import $ from 'jquery';
 $('#swiper').css('background-color', 'red')
 
-// 处理css
-import './styles/index.css'
+// // 处理css
+// import './styles/index.css'
+
+// 引入less样式
+import './styles/index.less';
+
+// 引入1张图片，生成一张图 挂到html
+import imgUrl from './assets/1.gif';
+
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+import imgUrl1 from './assets/logo_small.png';
+const img1 = document.createElement('img');
+img1.src = imgUrl1
+document.body.appendChild(img1)
--- webpack.config.js
+++ webpack.config.js
@@ -16,21 +16,67 @@ module.exports = {
             template: './public/index.html', //以我们自己的html文件作为模板去生成dist/html文件
             filename: 'index.html' // 生成文件的名称
         }), //插件-自动生成html文件
-        new CleanWebpackPlugin(), // 删除的是ouput path 里配置的那个输出文件的文件夹
+        // new CleanWebpackPlugin(), // 删除的是ouput path 里配置的那个输出文件的文件夹
     ],
     devServer: { //添加服务器配置
         port: 3000, // 端口号
         open: true
     },
-    module: { // loader 加载器 配置在这儿
+    module: { // loader 加载器
         rules: [ // loader的规则
             {
                 test: /\.css$/, // 匹配所有的css文件
                 // loader 执行的顺序： use数组里从右向左运行
-                // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
-                // 再用 style-loader 将样式, 把css插入到dom中
                 use: ["style-loader", "css-loader"]
-            }
+            },
+            {
+                test: /\.less$/, // 匹配执行类型的文件
+                // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+                // 执行的顺序 less-loader css-loader style-loader
+                use: ["style-loader", "css-loader", 'less-loader']
+            },
+            // { // webpack 4
+            //   test: /\.(png|jpg|gif)$/i, // 匹配的图片
+            //   // use: ['url-loader'],
+            //   // url-loader 转换based64 直接插入
+            //   // 不好的地方？？？based64 太大 如何解决
+
+            //   // 对图片的大小进行限制
+            //   // 配置limit, 超过 8 * 1024, 不转, file-loader复制, 随机名, 输出文件
+
+            //   // file-loader 直接复制图片 到dist目录下， 并且改改对应图片的src
+            //   use: [{ // 对当前的loader 进行配置
+            //     loader: 'url-loader',
+            //     options: {
+            //       limit: 8192,// 限制大小
+            //     },
+            //   }],
+            // },
+            {// webpack 5
+                test: /\.(png|jpg|gif)$/i, // 匹配的图片
+                // type: 'asset/resource', // 处理资源模块的方式
+                // asset/resource 直接复制dist 目录下
+                // type: 'asset/inline'
+                // 直接转换成based64
+
+                // 在导出一个 data URI 和发送一个单独的文件之间自动选择。
+                // 如果你设置的是asset模式
+                // 以8KB大小区分图片文件
+                // 小于8KB的, 把图片文件转base64, 打包进js中
+                // 大于8KB的, 直接把图片文件输出到dist下
+                type: 'asset',
+                dependency: { not: ['url'] },
+                // parser: { // 解析器 规则
+                //   dataUrlCondition: { // dataUrl的情况
+                //     maxSize: 8 * 1024,
+                //     // maxSize 限制最大值
+                //   },
+                // },
+                // generator: { // 生成器
+                //   filename: '[hash:6][ext]', // 资源文件处理之后 输出的文件名
+                //   // ext 文件扩展名
+                // }
+            },
         ]
     }
 }
