022c2f5d2cb943200f39db55bfa21f05fab7802c
Hayeszy <1282429235@qq.com>
2022-06-27 20:58:19 +0800

    新建了初始化项目, 配置入口和出口

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// 轮播图逻辑
+console.log('实现轮播图的业务逻辑')
+// tab栏切换的逻辑
+console.log('实现tabs标签页的逻辑')
+
+// 先引入jquery
+import $ from 'jquery';
+
+// 引入banner.js和tabs.js
+import './banner.js'
+import './tabs.js'
+
+$('#swiper').css('background-color', 'red')
